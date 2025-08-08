ab4bc7fe5aaeed3be2c85f0471a003cc72bb05d5
elaine <elainehu222@gmail.com>
2025-08-08 00:11:23 -0400

    debug

--- src/tsnkit/test/benchmark/__main__.py
+++ src/tsnkit/test/benchmark/__main__.py
@@ -163,14 +163,12 @@ if __name__ == "__main__":
                 try:
                     while sig.value < tasks:
                         time.sleep(1)
-                        print(sig.value)
                 except KeyboardInterrupt:
                     print(f"Terminate calculation by hand.")
                     tasks = sig.value
 
         print("exited")
         oom.terminate()
-        # oom.join()
         gc.collect()
 
         # add the processes that timed out to the results dataframe
