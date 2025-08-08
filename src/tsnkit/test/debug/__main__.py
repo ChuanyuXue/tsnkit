76678552e2a72186d6879bbdab7ecfa66e411170
Chuanyu Xue <chuanyu.xue@uconn.edu>
2025-08-08 13:56:40 -0400

    support default command call to test all algorithms

--- src/tsnkit/test/debug/__main__.py
+++ src/tsnkit/test/debug/__main__.py
@@ -1,13 +1,22 @@
 import argparse
+import os
 from ... import utils
 from .. import debug
-import os
+from ... import models
 
+def get_available_methods():
+    methods = []
+    models_dir = models.__path__[0]
+    for filename in os.listdir(models_dir):
+        if filename.endswith('.py') and not filename.startswith('_'):
+            modname = filename.replace('.py', '')
+            methods.append(modname)
+    return sorted(methods)
 
 def parse():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("methods", type=str, nargs="+", help="list of algorithms to be tested")
+    parser.add_argument("methods", type=str, nargs="?", help="list of algorithms to be tested")
     parser.add_argument("-t", type=int, default=utils.T_LIMIT, help="total timeout limit")
     parser.add_argument("-o", type=str, help="path for output report")
     parser.add_argument("--it", type=int, default=5, help="simulation iterations")
@@ -17,12 +26,10 @@ def parse():
 
 
 if __name__ == "__main__":
-    # script directory
-    script_dir = os.path.dirname(__file__)
-
-    # read command line args
     args = parse()
-    algorithms = args.methods
 
-    debug.run(algorithms, args)
+    if not args.methods:
+        args.methods = get_available_methods()
+
+    debug.run(args.methods, args)
 
