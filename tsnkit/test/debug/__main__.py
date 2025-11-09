70e17e9e291e39d80a71b8c349e1fda571d38eae
elaine <elainehu222@gmail.com>
2025-11-08 19:48:30 -0500

    allow selection of test cases for debugging

--- tsnkit/test/debug/__main__.py
+++ tsnkit/test/debug/__main__.py
@@ -17,6 +17,7 @@ def parse():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("methods", type=str, nargs="*", help="list of algorithms to be tested")
+    parser.add_argument("--ins", type=str, nargs="+", help="list of problem instances")
     parser.add_argument("-t", type=int, default=utils.T_LIMIT, help="total timeout limit")
     parser.add_argument("-o", type=str, help="path for output report")
     parser.add_argument("--it", type=int, default=5, help="simulation iterations")
